3335d8391ffb391d7a352dd10214e57a9bc43808
Maulik Patel <maulikp163@gmail.com>
2020-06-20 21:31:14 +0530

    initial commit

---
+++ src/main/java/com/bhavana/springbackend/SpringBackendApplication.java
@@ -0,0 +1,15 @@
+package com.bhavana.springbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class SpringBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bhavana/springbackend/SpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhavana.springbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
